13854639f4ba4b369da0a90f647506b6cb36ebc0
mvmedeiros <mvbmedeiros@gmail.com>
2025-07-03 20:15:14 +0200

    feat: add transaction creation method

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_transactions
+from .views import get_transactions, create_transaction
 
 urlpatterns = [
     path('transactions/', get_transactions, name='get_transactions'),
+    path('transactions/create/', create_transaction, name='create_transaction'),
 ]
